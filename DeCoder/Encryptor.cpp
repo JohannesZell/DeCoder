14566902af3dc4fa8b3c39231ed03c6fbba98698
Fabian Großmann <grossmann_fabian@web.de>
2019-01-22 12:34:45 +0100

    XOR Grundstein gelegt

--- DeCoder/Encryptor.cpp
+++ DeCoder/Encryptor.cpp
@@ -1,5 +1,8 @@
 #include "Encryptor.h"
+#include <iostream>
+#include <string.h>
 
+using namespace std;
 
 
 Encryptor::Encryptor()
@@ -13,6 +16,21 @@ Encryptor::~Encryptor()
 
 char * Encryptor::encrypt(char key[], char * rawData)
 {
-	return nullptr;
+	int keyLength = sizeof(key);
+	int remainingData = strlen(rawData);
+	char * encData;
+	int step = 0;
+	cout << remainingData;
+
+	while (remainingData > 0)
+	{
+		for (int count = 0; count < keyLength; count++) 
+		{
+			encData[step + count] = (int) rawData[step + count] ^ (int) key[count];
+		}
+		step = step + keyLength;
+	}
+
+	return encData;
 }
 
