ef4a47f7cab54c3f81ead0c2fb7f4e277e9b0921
Tailor Breeks <tbreeks@gmail.com>
2019-02-05 12:59:16 +0100

    CaesarEncIncluded

--- DeCoder/Encryptor.cpp
+++ DeCoder/Encryptor.cpp
@@ -1,8 +1,17 @@
 #include "Encryptor.h"
 #include <iostream>
-#include <string.h>
+#include <string>
+#include <fstream>
 
 using namespace std;
+
+char ch;
+
+//Array for PIN
+int cool[5];
+
+//Counter for looping the array
+int cc;
 
 
 Encryptor::Encryptor()
@@ -37,4 +46,76 @@ char * Encryptor::encryptXOR(char key[], char * rawData)
 
 	return __nullptr;
 }
+
+int encrypt(string sourcepath, string destpath) {
+	{
+		fstream fin(sourcepath, fstream::in);
+		fstream fout(destpath, fstream::out);
+		//Get PIN
+		cout << "Please enter your 5 digit PIN" << endl;
+
+		for (int i = 0; i < 5; i++)
+			cin >> cool[i];
+
+		cout << endl;
+
+		//Encrypt
+		while (fin >> noskipws >> ch) {
+			int ich = (int)ch;
+			ich = ich + cool[cc];
+			cc = ((++cc) % 5);
+			char ch2 = (char)ich;
+			cout << ch2;
+			fout << ch2;
+		}
+
+		cout << endl;
+		system("Pause");
+	}
+	return 0;
+}
+
+int decrypt(string sourcepath, string destpath) {
+	{
+		fstream fin(sourcepath, fstream::in);
+		fstream fout(destpath, fstream::out);
+		//Get PIN
+		cout << "Please enter your 5 digit PIN" << endl;
+
+		for (int i = 0; i < 5; i++)
+			cin >> cool[i];
+
+		cout << endl;
+
+		//Encrypt
+		while (fin >> noskipws >> ch) {
+			int ich = (int)ch;
+			ich = ich - cool[cc];
+			cc = ((++cc) % 5);
+			char ch2 = (char)ich;
+			cout << ch2;
+			fout << ch2;
+		}
+		cout << endl;
+		system("Pause");
+	}
+	return 0;
+}
+
+//int main() {
+//
+//	int choice;
+//	cout << "Please select your choice:" << endl << "1) encrypt" << endl << "2) decrypt" << endl;
+//
+//	cin >> choice;
+//
+//	if (choice == 1)
+//		encrypt("C:\\Users\\bepis\\Desktop\\test.txt", "C:\\Users\\bepis\\Desktop\\testneu.txt");
+//	else if (choice == 2)
+//		decrypt("C:\\Users\\bepis\\Desktop\\testneu.txt", "C:\\Users\\bepis\\Desktop\\testre.txt");
+//	else {
+//		cout << "Please enter valid number!" << endl;
+//		main();
+//	}
+//}
 
